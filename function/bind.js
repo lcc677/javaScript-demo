fb8725e65c2c2ae8b5eee456fc0773e9ffab37b0
刘晨晨 <chenchen@starmerx.com>
2020-08-03 09:11:16 +0800

    原型链

--- function/bind.js
+++ function/bind.js
@@ -12,3 +12,13 @@ console.log(unboundGetX()); // The function gets invoked at the global scope
 const boundGetX = unboundGetX.bind(moduless);
 console.log(boundGetX());
 // expected output: 42
+
+var add = function*(){
+  yield '1'
+  yield '2'
+}
+
+
+var yield = "9"
+console.log(yield)
+console.log(...add())
---
+++ 原型链.js
@@ -0,0 +1,22 @@
+function Student(){
+  this.name = "lcc"
+}
+Student.prototype.age = 12
+let st = new Student()
+console.log(st);
+console.log(st.__proto__);
+console.log(st.__proto__.__proto__);
+console.log(st.__proto__.__proto__.hasOwnProperty);
+console.log(Object.prototype.hasOwnProperty);
+
+class Person{
+  constructor(){
+    this.name="name"
+  }
+  add() {
+    console.log(99)
+  }
+}
+let person = new Person()
+console.log(person.hasOwnProperty('add'))
+console.log(Person.prototype.add)
